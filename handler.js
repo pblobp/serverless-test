da7a2d4cdc4b400e41c83fa0f602b894f2f6b235
Pablo <pblobp@gmail.com>
2019-03-31 17:21:10 -0300

    Converting to thumb and put into another bucket

--- handler.js
+++ handler.js
@@ -3,9 +3,11 @@ const uploadService = require("./services/uploadService");
 const thumbnailService = require("./services/thumbnailService");
 
 module.exports.uploadImage = async event => {
-  return uploadService.uploadImage(event);
+  const response = await uploadService.uploadImage(event);
+  if (response) return { statusCode: 201, body: response };
 };
 
 module.exports.generateThumbnail = async event => {
-  return thumbnailService.convertToThumbnail(event);
+  const response = await thumbnailService.convertToThumbnail(event);
+  if (response) return { statusCode: 201, body: response };
 };
--- services/thumbnailService.js
+++ services/thumbnailService.js
@@ -1,7 +1,26 @@
-
+const s3Service = require("../services/s3Service");
+const jimp = require("jimp");
+const { buckets } = require("../infra/config");
 
 const convertToThumbnail = async function(event) {
-    console.log("event > ", event);
+  const s3info = JSON.parse(event.Records[0].Sns.Message);
+  const bucket = s3info.Records[0].s3.bucket.name;
+  const key = s3info.Records[0].s3.object.key;
+  const objectS3 = await s3Service.getObject({
+    Bucket: bucket,
+    Key: key
+  });
+  const image = await jimp.read(objectS3.Body);
+  const buffer = await image
+    .resize(100, 100)
+    .quality(80)
+    .getBufferAsync(jimp.MIME_JPEG);
+  await s3Service.putObject({
+    Bucket: buckets.thumbnails,
+    Key: "thumb-" + key,
+    Body: buffer
+  });
+  return { key: "thumb-" + key, bucket: buckets.thumbnails };
 };
 
 module.exports = {
